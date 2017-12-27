60905c4e4aad32e04e818e49ef832552fc464358
Banie Setijoso <bsetijoso@fuze.com>
2017-12-27 02:02:31 -0500

    Observe the stream from InputFragment and update the title (for now)

--- app/src/main/java/com/banuu/android/jumlah/MainActivity.kt
+++ app/src/main/java/com/banuu/android/jumlah/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.banuu.android.jumlah
 
+import android.app.Fragment
+import android.app.FragmentManager
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import com.banuu.android.jumlah.input.view.InputFragment
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.main_cordinator.*
 
 class MainActivity : AppCompatActivity() {
+  private var inputStreamDisposable: Disposable? = null
+  private var totalSum = 0.0
+  private var totalLabel = ""
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main_cordinator)
     setSupportActionBar(toolbar)
+    supportActionBar?.setDisplayShowTitleEnabled(false)
 
     fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction("Action", null).show()
+      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction(
+          "Action", null).show()
+    }
+  }
+
+  override fun onStart() {
+    super.onStart()
+    val manager: FragmentManager = fragmentManager
+    var inputFragment: Fragment? = manager.findFragmentById(R.id.input_fragment)
+    if (inputFragment is InputFragment) {
+      inputStreamDisposable = inputFragment.cashInputStream
+          .observeOn(Schedulers.computation())
+          .map { computeSum(it) }
+          .observeOn(AndroidSchedulers.mainThread())
+          .subscribe {
+            toolbar.title = totalSum.toString()
+          }
+    }
+  }
+
+  override fun onStop() {
+    super.onStop()
+    inputStreamDisposable?.let { if (it.isDisposed) it.dispose() }
+  }
+
+  private fun computeSum(cashList: List<Pair<String, Double>>) {
+    totalSum = 0.0
+    for (cash in cashList) {
+      totalLabel += "(" + cash.first + ") + "
+      totalSum += cash.second
     }
   }
 }
