d95eff2fef43e9df7308cb753984997095006888
Banie Setijoso <bsetijoso@fuze.com>
2018-01-02 00:46:59 -0500

    Implement the Appbar layout to contain two textviews
    One textview is to show the breakdown while the other one is to show the total. When user scrolls up, the breakdown one will start to be hidden
    
    A drawback of this implementation is I'm unable to show the fab for some reason. Need to investigate more why it's not showing up

--- app/src/main/java/com/banuu/android/jumlah/MainActivity.kt
+++ app/src/main/java/com/banuu/android/jumlah/MainActivity.kt
@@ -3,7 +3,6 @@ package com.banuu.android.jumlah
 import android.app.Fragment
 import android.app.FragmentManager
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.banuu.android.jumlah.input.view.InputFragment
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -20,25 +19,27 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main_cordinator)
     setSupportActionBar(toolbar)
+
     supportActionBar?.setDisplayShowTitleEnabled(false)
 
-    fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction(
-          "Action", null).show()
-    }
+    //    fab.setOnClickListener { view ->
+    //      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction(
+    //          "Action", null).show()
+    //    }
   }
 
   override fun onStart() {
     super.onStart()
     val manager: FragmentManager = fragmentManager
-    var inputFragment: Fragment? = manager.findFragmentById(R.id.input_fragment)
+    val inputFragment: Fragment? = manager.findFragmentById(R.id.input_fragment)
     if (inputFragment is InputFragment) {
       inputStreamDisposable = inputFragment.cashInputStream
           .observeOn(Schedulers.computation())
           .map { computeSum(it) }
           .observeOn(AndroidSchedulers.mainThread())
           .subscribe {
-            toolbar.title = totalSum.toString()
+            sum_breakdown.text = totalLabel
+            sum_label.text = "$ " + totalSum.toString()
           }
     }
   }
@@ -51,7 +52,7 @@ class MainActivity : AppCompatActivity() {
   private fun computeSum(cashList: List<Pair<String, Double>>) {
     totalSum = 0.0
     for (cash in cashList) {
-      totalLabel += "(" + cash.first + ") + "
+      totalLabel += "(" + cash.first + ")" + if (cash != cashList.last()) " + " else ""
       totalSum += cash.second
     }
   }
